fa9f3327a4dcc262b2e9f4e8222cfc247698fecd
karineudahl <74554925+karineudahl@users.noreply.github.com>
2021-11-26 15:49:37 +0100

    update

--- js/pictureCarousel.js
+++ js/pictureCarousel.js
@@ -1,34 +1,35 @@
-const prev = document.querySelector(".prev");
-const next = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 const images = document.querySelector(".carousel").children;
 const totalImages = images.length;
 let index = 0;
 
-prev.addEventListener("click", () => {
+prevBtn.addEventListener("click", () => {
   nextImage("next");
 })
 
-next.addEventListener("click", () => {
+nextBtn.addEventListener("click", () => {
   nextImage("prev");
 })
 
 function nextImage(direction) {
-  if(direction === "next") {
-    index++;
-    if(index === totalImages) {
-      index = 0;
+    if(direction === "next") {
+            index++;
+        if(index === totalImages) {
+            index = 0;
+        }
+    } 
+    else {
+        if(index === 0) {
+            index = totalImages - 1;
+        } else {
+            index--;
+        }
     }
-  } else {
-    if(index === 0) {
-      index = totalImages - 1;
-    } else {
-      index--;
-    }
-  }
 
-  for(let i = 0; i < images.length; i++) {
-    images[i].classList.remove("main");
-  }
-  images[index].classList.add("main");
+    for(let i = 0; i < images.length; i++) {
+        images[i].classList.remove("main");
+    }
+
+    images[index].classList.add("main");
 }
-
